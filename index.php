61caae6ff5a522ac4e0c903ff8c7383602032369
Issa Diao <diaois01@MA2058.local>
2014-05-13 13:48:25 -0700

    two

--- index.php
+++ index.php
@@ -1,5 +1,13 @@
 <?php
 
-echo "hello world";
+require_once 'lib/Twig/Autoloader.php';
+Twig_Autoloader::register();
+
+$loader = new Twig_Loader_Filesystem('templates');
+$twig = new Twig_Environment($loader, array(
+  'cache' => 'cache',
+));
+
+echo $twig->render('index.html', array());
 
 ?>
