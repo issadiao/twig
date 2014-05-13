fa61490c18426413969c13fff0c8c451e5e90f95
Issa Diao <diaois01@MA2058.local>
2014-05-13 13:56:43 -0700

    test

--- index.php
+++ index.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once '../lib/Twig/Autoloader.php';
+require_once 'lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem('../templates');
+$loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
   'cache' => 'cache',
 ));
 
 echo $twig->render('index.html', array());
+
 
 ?>
