4b66922e92fc5c2e78d27001cc922760b0901f87
Issa Diao <diaois01@MA2058.local>
2014-05-13 13:50:33 -0700

    two

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
-require_once 'lib/Twig/Autoloader.php';
+require_once '../lib/Twig/Autoloader.php';
 Twig_Autoloader::register();
 
-$loader = new Twig_Loader_Filesystem('templates');
+$loader = new Twig_Loader_Filesystem('../templates');
 $twig = new Twig_Environment($loader, array(
   'cache' => 'cache',
 ));
