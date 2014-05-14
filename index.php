572ba3fbc1060236c4eda6d11aead4aefb0168a4
Issa Diao <diaois01@MA2058.local>
2014-05-14 11:20:04 -0700

    api

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 require_once 'lib/Twig/Autoloader.php';
+require_once 'functions.inc';
+
 Twig_Autoloader::register();
 
 $loader = new Twig_Loader_Filesystem('templates');
@@ -8,7 +10,14 @@ $twig = new Twig_Environment($loader, array(
  // 'cache' => 'cache',
 ));
 
-echo $twig->render('news.twig', array());
+$url = explode("?", $_SERVER['REQUEST_URI']);
+$section = explode("/", $url[0]);
+
+$variables = thr_twig_get_variables($url[0], $section[1]);
+
+$section = $section[1] . '.twig';
+
+echo $twig->render($section, $variables);
 
 
 ?>
