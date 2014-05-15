eea8d166ce7bb282937bb784cba63aa4a2f4b85d
Issa Diao <diaois01@MA2058.local>
2014-05-14 17:33:06 -0700

    api

--- index.php
+++ index.php
@@ -8,7 +8,9 @@ Twig_Autoloader::register();
 $loader = new Twig_Loader_Filesystem('templates');
 $twig = new Twig_Environment($loader, array(
  // 'cache' => 'cache',
+  "debug" => TRUE,
 ));
+$twig->addExtension(new Twig_Extension_Debug());
 
 $url = explode("?", $_SERVER['REQUEST_URI']);
 $section = explode("/", $url[0]);
@@ -18,6 +20,5 @@ $variables = thr_twig_get_variables($url[0], $section[1]);
 $section = $section[1] . '.twig';
 
 echo $twig->render($section, $variables);
-
 
 ?>
